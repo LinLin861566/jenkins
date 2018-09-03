573d366754530ea787dcb5e6a8c76dda2b88f3ab
linlin <469923498@qq.com>
2018-09-03 16:42:21 +0800

    test push all files

---
+++ src/main/java/com/ll/demo/service/interfaces/LoginService.java
@@ -0,0 +1,9 @@
+package com.ll.demo.service.interfaces;
+
+import com.ll.demo.model.login.LoginUser;
+
+import java.util.Map;
+
+public interface LoginService {
+    public String login (Map<String,Object> param);
+}
